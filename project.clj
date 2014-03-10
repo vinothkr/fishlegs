db7226e79c43e641cb0f346aed2c7bab63e84261
VinothKumar Raman <vinoth@indix.com>
2014-03-10 15:02:26 +0530

    Adding incanter dep

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject fishlegs "0.1.0-SNAPSHOT"
   :description "Metric collection and aggregation service"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.novemberain/monger "1.7.0"]])
+                 [com.novemberain/monger "1.7.0"]
+                 [incanter "1.5.1"]])
